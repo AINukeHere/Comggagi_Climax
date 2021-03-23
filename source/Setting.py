65b627ac7371b65f563160e1ed503c2e198aaaca
AINukeHere <iii4625@naver.com>
2021-03-23 22:38:22 +0900

    Apply gitignore

--- source/Setting.py
+++
@@ -1,8 +0,0 @@
-_DEBUG = False
-#_DEBUG = True
-ENTRANCE_ATTACKER_PRINT = False
-# ENTRANCE_ATTACKER_PRINT = True
-CHEAT_DEBUG = False
-#CHEAT_DEBUG = True
-ReaverFixDebug = False
-#ReaverFixDebug = True
--- source/unused/eudTurbo.py
+++
@@ -1,6 +0,0 @@
-from eudplib import *
-def afterTriggerExec():
-    DoActions([
-        # eudTurbo
-        SetMemory(0x6509A0, SetTo, 0),
-    ])
